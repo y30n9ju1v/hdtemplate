4bb00c485d0ab4aa380aa78e219e26f226e12806
ecourse <smkang@codenuri.co.kr>
2022-10-04 13:07:55 +0900

    Add files via upload

--- DAY1/3_dependent_name3.cpp
+++ DAY1/3_dependent_name3.cpp
@@ -10,9 +10,28 @@ struct Object
 		std::cout << "template mf" << std::endl;
 	}
 };
+
+// template specialization 문법
+template<>
+struct Object<int>
+{
+	static void mf()
+	{
+		std::cout << "mf" << std::endl;
+	}
+};
+template<>
+struct Object<double>
+{
+	static constexpr int mf = 0;
+};
 int main()
 {
-	Object<int>::mf<int>();
-	Object<double>::mf<int>();
+	// Object<T>에서 어떤 T가 와도 object template을 
+	// 보면 mf가  template이라는걸 알 수 있지 않나요?
+	// => template specialization 문법 이 있기 때문에
+	// => 템플릿 인자에 따라서 "mf" 의 의미가 달라질수 있습니다.   
+	Object<int>::mf();
+	Object<double>::mf;
 	Object<char>::mf<int>();
 }
