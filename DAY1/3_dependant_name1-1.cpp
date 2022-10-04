ab3bfb0cc564be87108115c976cc39ddfa49dc71
ecourse <smkang@codenuri.co.kr>
2022-10-04 11:31:28 +0900

    Add files via upload

--- DAY1/3_dependant_name1-1.cpp
+++ DAY1/3_dependant_name1-1.cpp
@@ -14,11 +14,34 @@ struct Object
 template<typename T>
 void foo(T obj)
 {
+	// ::value 는 값 입니다. typename 필요 없습니다.
 	Object::value * 10;
-	Object::type* p1;
+	T::value * 10;			// <= value 는 타입이 아니므로
+							//    typename 없어야합니다.
 
-	Object::rebind<int> a;
+	Object::type* p1;	 // "dependant name" 아닙니다. typename필요없음
+	typename T::type* p2;// "dependant name" 입니다. typename 필요
+	//==========================================================
+	// rebind 는 Object 안에 내포된 구조체 입니다.
+	Object::rebind<int> r1; // ok. dependant name 아님.
+
+//	T::rebind<int> r2;	// error
+						// rebind 는 dependant name 이므로
+						// typename 필요
+
+//	typename T::rebind<int> r2;	// error
+								// rebind 의 선언을 조사할수 없으므로
+								// "rebind<" 표기에서 "<" 의 의미를
+								// 알수 없기 때문에
+
+	typename T::template rebind<int> r2; // ok.. rebind가 
+								// 템플릿 임을 알려주어야 합니다.
+
+	// 결론 : 위 코드에서 "typename", "template" 이 필요한 이유 알아두세요
 }
+
+
+
 
 int main()
 {
