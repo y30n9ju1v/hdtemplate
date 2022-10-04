c0d1bb76daeb6c0130c09efc1e0b95dda2aee2eb
ecourse <smkang@codenuri.co.kr>
2022-10-04 16:40:32 +0900

    Add files via upload

--- DAY1/7_template_overloading2.cpp
+++ DAY1/7_template_overloading2.cpp
@@ -4,16 +4,22 @@
 // overloading1 번은 
 // => "int 타입" 과 "int가 아닌타입" 을 다르게 처리하는 코드입니다.
 
-// "정수계열" 과 "실수계열"을 다르게 처리하고 싶다면.. 
 
-template<typename T> 
+// "정수계열" 과 "실수계열"을 다르게 처리하고 싶다면.. 
+// => 유명한 요구 조건 입니다.
+// => C++20 이전 : enable_if, true_type/false_type, if constexpr로해결
+// => C++20 : concept 으로 해결
+// 모두 내일 부터 배우는 기술
+
+// 아래 코드는 "C++20" 입니다. 
+template<typename T> requires std::integral<T>
 T square(T a)
 {
 	std::cout << "integral" << std::endl;
 	return a * a;
 }
 
-template<typename T> 
+template<typename T> requires (!std::integral<T>)
 T square(T a)
 {
 	std::cout << "not integral" << std::endl;
@@ -24,4 +30,6 @@ int main()
 {
 	square(3);  
 	square(3.4);
+	square(3.4f); // float 도 실수 계열이므로 아래 버전 사용
+	
 }
