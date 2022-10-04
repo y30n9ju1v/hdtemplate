892d26488adf6bee789ba12c69168f7e4bea1342
ecourse <smkang@codenuri.co.kr>
2022-10-04 15:45:57 +0900

    Add files via upload

--- DAY1/5_lazy_instantiation5-1.cpp
+++ DAY1/5_lazy_instantiation5-1.cpp
@@ -5,7 +5,8 @@ class CarStaticInit
 public:
 	CarStaticInit() { std::cout << "Car 클래스 생성자" << std::endl; }
 };
-
+// 아래처럼 Car 를 만들면 위 생성자가 호출되지만
+// "template" 으로 변경하면 호출되지 않습니다. "지연된 인스턴스화" 때문에..
 class Car
 {
 public:
