38d7df4febefb67e219ace6c2190cfb01999ffd2
ecourse <smkang@codenuri.co.kr>
2022-10-04 14:50:11 +0900

    Add files via upload

--- DAY1/4_instantiation5-1.cpp
+++ DAY1/4_instantiation5-1.cpp
@@ -15,6 +15,12 @@ void goo(T a, typename std::type_identity<T>::type  b)
 
 int main()
 {
-	foo(3.4, 10);
-	goo(3.4, 10);
+	foo(3.4, 10);	// error. 타입 전달을 안했으므로 "함수인자로 결정"해야 하는데
+					// 1번째 인자를 보면 "double"
+					// 2번째 인자를 보면 "int" 이므로 타입 결정 안됨
+
+	goo(3.4, 10);	// ok.. 1번째 인자로만 타입 결정
+					// 2번째 인자의 타입은 무조건 1번째와 같은 타입
+					// goo(double, double) 로 결정
+					// 2번째 인자는 "10" 은 double로 전달 가능
 }
