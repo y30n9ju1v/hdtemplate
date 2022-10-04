09c6f52e3a2b86b1d66936bd29ad43da77669c70
ecourse <smkang@codenuri.co.kr>
2022-10-04 13:28:08 +0900

    Add files via upload

--- DAY1/4_instantiation2-1.cpp
+++ DAY1/4_instantiation2-1.cpp
@@ -1,6 +1,7 @@
 // square.h 를 만들고 아래 코드 넣으세요
 template<typename T> T square(T a);
 
+//==================================================
 // square.cpp 를 만들고 아래 코드 넣으세요
 #include "square.h"
 
@@ -8,6 +9,14 @@ template<typename T> T square(T a)
 {
 	return a * a;
 }
+
+// template 을 별도의 구현파일에 넣으려면
+// 아래 처럼 explicit instantiation 을 사용하면 됩니다.
+template int    square(int a);
+template double square(double a);
+//===========================================
+
+
 
 // 4_instantiation2-1.cpp 에서 아래 처럼 만드세요
 #include <iostream>
