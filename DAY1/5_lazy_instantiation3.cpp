a2914194dcbd92e3e06c2fb0f5af13d00c527bee
ecourse <smkang@codenuri.co.kr>
2022-10-04 15:16:54 +0900

    Add files via upload

--- DAY1/5_lazy_instantiation3.cpp
+++ DAY1/5_lazy_instantiation3.cpp
@@ -6,8 +6,10 @@ class queue
 {
 	std::vector<int> c;
 public:
-	void push(int a)  {	c.push_back(a);	}
-	void pop() 		  {	c.pop_front();	}
+	// 핵심 2. 현재 queue 는템플릿이 아닙니다.
+	// 멤버 함수를 사용하지 않아도 "잘못된 코드가 있으면 에러" 입니다.
+	void push(int a)  {	c.push_back(a);	} // ok
+	void pop() 		  {	c.pop_front();	} // error
 };
 
 int main()
