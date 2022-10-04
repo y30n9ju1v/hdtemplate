72fa944db2df21d3fa0e8ae1d6c4041a2f88453c
ecourse <smkang@codenuri.co.kr>
2022-10-04 15:43:44 +0900

    Add files via upload

---
+++ DAY1/5_lazy_instantiation5-1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class CarStaticInit
+{
+public:
+	CarStaticInit() { std::cout << "Car 클래스 생성자" << std::endl; }
+};
+
+class Car
+{
+public:
+	Car() {} // 생성자.. 객체당 한번씩 호출됩니다.
+
+//	static Car() {} // C# 언어가 가지는 static 생성자 개념	
+					// Car 클래스에 대해서 최초에 1회 호출됩니다.
+					// "클래스 생성자"라고 합니다.
+					// C++에 이문법이 없습니다.
+	static CarStaticInit init; // init 의 생성자가 결국 Car 클래스에 대해	
+								// 한번 호출됩니다.
+};
+CarStaticInit Car::init;
+
+int main()
+{
+	Car c1;
+	Car c2;
+
+}
