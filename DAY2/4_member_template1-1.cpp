3e6e81977caab5c53a580659cf23954555f09fd9
ecourse <smkang@codenuri.co.kr>
2022-10-05 11:39:36 +0900

    Add files via upload

--- DAY2/4_member_template1-1.cpp
+++ DAY2/4_member_template1-1.cpp
@@ -11,13 +11,19 @@ public:
 };
 
 // mf1, mf2, mf3 외부 구현 해 보세요.
-void Object::mf1(int n) 
+
+template<typename T>
+void Object<T>::mf1(int n) 
 {
 }
-void Object::mf2(T n)
+template<typename T>
+void Object<T>::mf2(T n)
 {
 }
-void Object::mf3(U n)
+
+//template<typename T, typename U>			// 1 => 이 표기는 클래스 템플릿의 인자가 2개라는 의미
+template<typename T> template<typename U>	// 2 => 답. "클래스도 템플릿 인데, 멤버함수도 템플릿 이라는 의미"
+void Object<T>::mf3(U n)
 {
 }
 
