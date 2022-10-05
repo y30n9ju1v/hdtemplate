ffb4a111ca2c1e790149e39062c9fb2957e86532
ecourse <smkang@codenuri.co.kr>
2022-10-05 16:12:27 +0900

    Add files via upload

---
+++ DAY2/9_specialization2.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+// 핵심 : 문법 연습
+
+template<typename T, typename U> struct Object
+{
+	static void fn() { std::cout << "T, U" << std::endl; }
+};
+
+// T*, U*
+template<typename T, typename U> struct Object<T*, U*>
+{
+	static void fn() { std::cout << "T*, U*" << std::endl; }
+};
+
+// T*, U
+template<typename T, typename U> struct Object<T*, U>
+{
+	static void fn() { std::cout << "T*, U" << std::endl; }
+};
+
+// 핵심 : T, T , 즉, 2개타입이 동일 타입일때
+
+// primary template 의 타입인자가 2개 라면
+
+// 1. 사용자는 반드시 타입인자를 2개 표기해야 합니다. : Object<int, double>
+
+// 2. 부분 특수화의 오른쪽 끝(A부분)도 꼭 2개 표기해야 합니다.
+
+// 3. 하지만, 부분 특수화 버전의 템플릿 인자 갯수를 2개가 아닐수도 있습니다.
+
+template<typename T> struct Object<T, T>   // <=== "A"
+{
+	static void fn() { std::cout << "T, T" << std::endl; }
+};
+
+// int, U 만들어 보세요
+template<typename U> struct Object<int, U>
+{
+	static void fn() { std::cout << "int, U" << std::endl; }
+};
+
+// int, short
+// => 이 경우는 "부분 특수화가 아닙니다."
+// => 특수화(specialization) 입니다. 모든 타입이 확정된 경우.
+template<> struct Object<int, short>
+{
+	static void fn() { std::cout << "int, short" << std::endl; }
+};
+
+int main()
+{
+	Object<char, double>::fn();	  // T, U
+	Object<char*, double*>::fn(); // T*, U*	
+	Object<short*, double>::fn(); // T*, U
+	Object<float,  float>::fn();  // T, T
+	Object<int,    float>::fn();  // int, U
+
+	Object<int, short>::fn();	  // int, short
+
+}
