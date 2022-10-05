00d1d8a790f987e7244a1dae03452cf081a86bc3
ecourse <smkang@codenuri.co.kr>
2022-10-05 11:23:08 +0900

    Add files via upload

--- DAY2/3_class_template_basic3.cpp
+++ DAY2/3_class_template_basic3.cpp
@@ -8,13 +8,16 @@ struct Object
 
 	Object(const T1& a, const T2& b);
 };
-
 // 1. Object 의 static 변수인 cnt 를 외부 선언해 보세요
-
+template<typename T1, typename T2> int Object<T1, T2>::cnt;
 
 
 // 2. Object 의 생성자 구현부 만들어 보세요 "first(a), second(b)"로 초기화 하세요
-
+template<typename T1, typename T2>
+Object<T1, T2>::Object(const T1& a, const T2& b)
+	: first(a), second(d)
+{
+}
 
 
 
