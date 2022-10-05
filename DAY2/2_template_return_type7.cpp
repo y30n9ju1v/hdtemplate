51588bfdbf119ff2c32e01b26d4eaf75257c1ab7
ecourse <smkang@codenuri.co.kr>
2022-10-05 10:47:20 +0900

    Add files via upload

--- DAY2/2_template_return_type7.cpp
+++ DAY2/2_template_return_type7.cpp
@@ -7,11 +7,9 @@
 // 아래 코드는 "template_return_type6.cpp" 의 add 와 완벽히 동일합니다.
 // 단, C++20 부터 가능
 // 아래 코드에서 a, b 는 서로 다른 템플릿 입니다.
-auto add(const auto& a, const auto& b)
+auto add(const auto& a, const auto & b)
 {
 	return a + b;
-
-
 }
 
 int main()
