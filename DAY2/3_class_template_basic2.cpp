0f2127424fb3387e88b5560bc29dcba46cbf6ada
ecourse <smkang@codenuri.co.kr>
2022-10-05 11:18:53 +0900

    Add files via upload

--- DAY2/3_class_template_basic2.cpp
+++ DAY2/3_class_template_basic2.cpp
@@ -17,6 +17,11 @@ public:
 //     2. "T" 가 템플릿 인자 임을 알리기 위해 모든 멤버 함수 앞에
 //       "template<typename T>" 가 있어야 합니다.
 
+//     3. 멤버 함수는 클래스 템플릿 외부에 만들수는 있지만
+//        별도의 소스 파일로 분리하지 마세요
+//		  반드시, 헤더 안에 "모든 멤버 함수 구현부" 가 있어야 합니다.
+
+
 template<typename T> 
 Vector<T>::Vector(std::size_t sz) : size(sz)
 {
