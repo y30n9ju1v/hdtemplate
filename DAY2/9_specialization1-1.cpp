273c5195912251bd379a0545b0c402f20801cb5a
ecourse <smkang@codenuri.co.kr>
2022-10-05 15:47:36 +0900

    Add files via upload

--- DAY2/9_specialization1-1.cpp
+++ DAY2/9_specialization1-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 
+// "primary template" 이라고 합니다.
 template<typename T> class Vector
 {
 	T* ptr;
@@ -14,7 +15,8 @@ public:
 	}
 	~Vector() { delete[] ptr; }
 };
-
+// specialization (특수화, 전문화) 라고 합니다.
+// => 타입이 완전히 확정된 경우
 template<> class Vector<bool>
 {
 	int* ptr;
@@ -29,11 +31,30 @@ public:
 	}
 	~Vector() { delete[] ptr; }
 };
+// partial specialization(부분 특수화, 부분 전문화)
+template<typename T> class Vector<T*>
+{
+	// 포인터에 최적화 되도록 구현
+public:
+	Vector(std::size_t sz) : size(sz)
+	{
+		std::cout << "T* 버전 사용" << std::endl;
+	}
+	~Vector() { }
+};
 
 int main()
 {
 	Vector<int>    v1(5);	
 	Vector<double> v2(5);   
 	Vector<bool>   v3(100); 
+
+	Vector<int*>   v4(5);
+	Vector<double*>v5(100);
 }
 
+// 참고. 실제 std::vector 는
+// primary template 과
+// vector<bool> 특수화 버전으로만 되어 있습니다.
+
+// 위 코드 처럼 포인터에 대한 부분 특수화가 되어 있지는 않습니다.
