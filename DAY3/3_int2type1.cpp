a36f9057098da484720c5013a185eb22ddf9c758
ecourse <smkang@codenuri.co.kr>
2022-10-06 10:39:37 +0900

    Add files via upload

--- DAY3/3_int2type1.cpp
+++ DAY3/3_int2type1.cpp
@@ -11,26 +11,30 @@ template<typename T> void pointer(const T& value)
 {
 	std::cout << value << " : " << *value << std::endl;
 }
-
 template<typename T> void not_pointer(const T& value)
 {
 	std::cout << value << std::endl;
 }
 
-
-
-
-
-
 template<typename T>
 void printv(const T& value)
 {
-	if (std::is_pointer_v<T>)  
-
-		std::cout << value << " : " << *value << std::endl;
+	// 의도 : T = int 라면 if ( false ) 이고,  pointer(v) 를 사용되지 않는다.
+	//      따라서, 인스턴스화 되지 않을것이다 !!
+	if (std::is_pointer_v<T>)
+		pointer(v);
 	else
-		std::cout << value << std::endl;
+		not_pointer(v);
+
+	// 실패!!
+	// if 는 실행시간 조건문이므로 조건식의 결과에 상관없이
+	// pointer(v), not_pointer(v) 모두 인스턴스화가 된다.!!!
+	// T=int 일때 pointer(v)가 인스턴스화 되므로 에러!!
+
+	// 의도대로 하려면 "if" 대신 "if constexpr(C++17)" 필요
 }
+
+
 
 int main()
 {
