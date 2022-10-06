c04e5d627ab20d348711c3072ebc054f80765644
ecourse <smkang@codenuri.co.kr>
2022-10-06 14:22:52 +0900

    Add files via upload

---
+++ DAY3/6_enable_if6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+class Animal {};
+class Dog : public Animal {};
+
+template<typename T> class smart_ptr
+{
+	T* obj = nullptr;
+public:
+	explicit smart_ptr(T* p) : obj(p) {}
+
+	// Generic copy constructor
+	template<typename U>
+	smart_ptr(const smart_ptr<U>& sp) : obj(sp.obj) {}
+
+
+	template<typename> friend class smart_ptr;
+};
+
+int main()
+{
+	smart_ptr<Dog>    p1(new Dog);
+	smart_ptr<Animal> p2 = p1;	// Animal* p2 = &Dog 개념이므로 되야합니다.
+								// p1 : smart_pointer<Dog>
+								// p2 : smart_pointer<Animal>
+								// => 어제 배운 Generic 복사 생성자필요
+						   
+}
+
+
