32d2f06dbf936ac4970867b483f3ed59434af378
ecourse <smkang@codenuri.co.kr>
2022-10-06 13:39:03 +0900

    Add files via upload

--- DAY3/6_enable_if2.cpp
+++ DAY3/6_enable_if2.cpp
@@ -9,16 +9,21 @@ template<typename T> T gcd(T a, T b)
 
 	return 0;// 구현은 생략.. 
 }
-
 double gcd(double a, double b)
 {
 	std::cout << "double version" << std::endl;
 	return 0;
 }
-
 int main()
 {
 	gcd(6, 9);
+	gcd(3.1,  5.4);		// double 버전 사용.
+	gcd(3.1f, 5.4f);	// T 버전 사용
+						// 그런데, 정수 타입이 아니므로 컴파일 에러
 
-	gcd(3.1,  5.4);
+	// 핵심
+	// static_assert : 조건을 만족하지 않으면 error!!
+	// 
+	// => 조건을 만족하지 않을때, 에러내지 말고, 후보에서만 제외할수 없을까 ?
+	// => 정수가 아닌경우, double 을 사용할수 있도록
 }
