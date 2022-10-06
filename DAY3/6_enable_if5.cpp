568ec0d1c77d964c725c0883c5d1e424a9e40957
ecourse <smkang@codenuri.co.kr>
2022-10-06 14:18:30 +0900

    Add files via upload

--- DAY3/6_enable_if5.cpp
+++ DAY3/6_enable_if5.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <type_traits>
 
+// 기존 템플릿 개념 : 모든 타입에 동작하는 함수
+//                 (모든 타입에 대한 함수 생성)
+// 
+// enable_if 개념 : 조건을 만족하는 타입에 대해 동작하는 함수
+//					(조건을 만족하는 타입에 대해서만 함수를 생성)
+//					조건을 만족하지 않으면 에러가 아니라, 사용안함!
+
 template<typename T>
-? T가 포인터인 경우만 사용
+std::enable_if_t< std::is_pointer_v<T> >
 printv(const T& value)
 {
 	std::cout << value << " : " << *value << std::endl;
 }
 
 template<typename T>
-? T가 포인터가 아닌 경우만 사용
+std::enable_if_t< !std::is_pointer_v<T> >
 printv(const T& value)
 {
 	std::cout << value << std::endl;
 }
-
-
 
 int main()
 {
